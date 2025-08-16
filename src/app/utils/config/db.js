b59ad3ee3a230b427ca4aa5f780a4e02867517f1
Akrishna <Akrishna@nba.com>
2025-08-16 14:36:06 +0530

    db changes

--- src/app/utils/config/db.js
+++ src/app/utils/config/db.js
@@ -4,6 +4,7 @@ const { default: mongoose } = require("mongoose");
 const DBConnection = async()=>{
     try {
         await mongoose.connect(process.env.MONGO_URI);
+        console.log("Database connected successfully");
     } catch (error) {
         console.log(error)
     }
